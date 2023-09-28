60b80909ba34a427b40e04a48fe29dff7047c868
Andrew Morrison <morro141@gmail.com>
2023-09-27 22:59:45 -0400

    apollo stuff?

---
+++ lib/apollo-wrapper.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  NextSSRApolloClient,
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import { setVerbosity } from "ts-invariant";
+
+if (process.env.NODE_ENV === "development") {
+  setVerbosity("debug");
+  loadDevMessages();
+  loadErrorMessages();
+}
+
+const getToken = () =>
+  document?.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
+
+const token = getToken();
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: "http://localhost:3001/graphql",
+    headers: {
+      'X-CSRF-Token': token || "",
+    },
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            // in a SSR environment, if you use multipart features like
+            // @defer, you need to decide how to handle these.
+            // This strips all interfaces with a `@defer` directive from your queries.
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+export const ApolloWrapper = ({ children }: React.PropsWithChildren) => {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
