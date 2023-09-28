805d9ab32b4d5e960746970dc281b132becc9aea
Andrew Morrison <morro141@gmail.com>
2023-09-27 23:00:00 -0400

    all the great stuff

--- next.config.js
+++ next.config.js
@@ -1,4 +1,48 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'localhost',
+      'lh3.googleusercontent.com',
+    ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'graphql-tag/loader',
+        },
+      ],
+    });
+    
+    return config;
+  },
+};
 
 module.exports = nextConfig
