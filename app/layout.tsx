ad869966f242466a272e9034c70a19228f4d94f5
Andrew Morrison <morro141@gmail.com>
2023-10-01 22:21:02 -0400

    add toast styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ToastProvider } from "@/components/providers/ToastProvider";
 import { ApolloWrapper } from "@/lib/apollo-wrapper";
+import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
