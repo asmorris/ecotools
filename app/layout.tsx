08b7edab2b3fc6fbd311724631c5b1ccf400dbac
Andrew Morrison <morro141@gmail.com>
2023-10-03 22:56:45 -0400

    remove toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ToastProvider } from "@/components/providers/ToastProvider";
 import { ApolloWrapper } from "@/lib/apollo-wrapper";
-import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
