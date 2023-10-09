689329e0c97f8b0f5080c5559a48d0790a1e9152
Andrew Morrison <morro141@gmail.com>
2023-10-08 21:59:50 -0400

    add side/navbars

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { MobileSidebar } from "./MobileSidebar"
+
+export const Navbar = () => {
+  return (
+    <div className="flex items-center p-4 w-full h-24">
+      <MobileSidebar />
+    </div>
+  )
+}
