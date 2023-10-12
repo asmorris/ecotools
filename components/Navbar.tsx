2a124f77d06244b22c5324ee8afe5c5694bfe9cd
Andrew Morrison <morro141@gmail.com>
2023-10-11 22:38:36 -0400

    nav and sidebar changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,13 @@
+import { UserButton } from "@clerk/nextjs"
 import { MobileSidebar } from "./MobileSidebar"
 
 export const Navbar = () => {
   return (
     <div className="flex items-center p-4 w-full h-24">
       <MobileSidebar />
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
   )
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,9 +10,9 @@ import {
   LayoutDashboard,
   MessageSquare,
   Settings,
-  Wrench,
 } from "lucide-react";
 import { Montserrat } from "next/font/google";
+import Image from "next/image";
 
 const montserrat = Montserrat({
   weight: "600",
@@ -60,7 +60,7 @@ export const Sidebar = () => {
       <div className="px-3 py-2 flex-1">
         <Link href="/" className="flex items-center pl-3 mb-14">
           <div className="relative w-8 h-8 mr-4">
-            <Wrench />
+            <Image width="50" height="50" src="/logo.svg" alt='Ecotools logo'/>
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>
             EcoTools
