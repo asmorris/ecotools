12c3462c1d866572bfcc9f4534617c303ad03f1d
Andrew Morrison <morro141@gmail.com>
2023-10-11 22:38:24 -0400

    loader

---
+++ components/Loader.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+
+export const Loader = () => {
+  return (
+    <div className="bg-slate-800 h-full flex flex-col gap-y-4 items-center justify-center">
+      <div className="w-10 h-10 relative animate-spin duration-[2000ms]">
+        <Image alt="logo" fill src="/logo.svg" />
+      </div>
+      <p className="text-sm text-slate-300 bg-slate-800">We on this</p>
+    </div>
+  );
+};
