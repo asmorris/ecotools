deb2a2d0e4ec78e4de67996fadaa4458dea1d502
Andrew Morrison <morro141@gmail.com>
2023-10-11 22:37:04 -0400

    add clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ToastProvider } from "@/components/providers/ToastProvider";
-import { ApolloWrapper } from "@/lib/apollo-wrapper";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,15 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ApolloWrapper>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
           <ToastProvider />
           <main className="flex w-full h-full text-background bg-foreground">
             {children}
           </main>
-        </ApolloWrapper>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
